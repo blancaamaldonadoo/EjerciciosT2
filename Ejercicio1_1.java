60279293a929a273801f790186700dae93cd8910
blancaamaldonadoo <159050395+blancaamaldonadoo@users.noreply.github.com>
2024-03-02 08:34:10 +0000

    cambios

---
+++ Ejercicio1_1.java
@@ -0,0 +1,39 @@
+/*EJERCICIO1.1
+Investigación y Análisis Crítico: Investiga tres lenguajes de programación: uno procedimental (como C), 
+uno orientado a objetos (como Java), y uno que soporte multiparadigma (como Python). 
+Describe cómo cada uno maneja la abstracción y cómo ha evolucionado este concepto a lo largo del tiempo. 
+Analiza cómo la abstracción beneficia al desarrollo de software en términos de mantenibilidad, 
+escalabilidad y legibilidad.
+
+
+
+En un lenguaje de programación procedimental como C, la abstracción depende en gran parte de los términos
+de la estructura del ordenador más que en los términos del problema que se tiene que resolver.
+
+En en un lenguaje de programación orientada a objetos como Java, se proporciona la creación de objetos
+para que el usuario costruya las abstracciones que le permitan modelar el problema lo más
+adecuadamente posible. De este modo, el lenguaje se adapta a cada problema creando las abstracciones 
+que mejor se adapten a él.
+En este tipo de lenguajes, se permite crear nuevas estructuras partiendo de las existentes.
+
+En lenguajes como Python, las librerias y módulos evitan que el programador tenga que ser conocedor de
+las implementaciones. EL propgramador simlpemente usa las funciones y métodos que les proporcionan las 
+distintas librerías.
+
+Esta se podría considerar como una línea del tiempo en la que observamos cómo la abstracción
+permite la flexibilididad y reducción de complejidad en la elaboración de un programa.
+El aspecto más importante es la gran independencia que se ha logrado alcanzar entre la máquina y el sistema.
+Es decir, cada vez el software de un ordenador era más ajustable a una abstracción que no necesitase
+su "aprobación".
+
+En la programación modular se consigue aumentar la idependencia y reutilización de código; e incorporar
+el soporte ara definir la parte pública y la privada.
+Por otro lado, en la programación orientada a objetos, los datos pueden llegar a compartir
+un código común (herencia) e incluso adpatar un código compartido a cada tipo de dato (polimorfismo).
+
+La complejidad de un sistema cuanto menor es el campo de la abstracción aumenta, es decir,
+hay mayor dificultad en su legibilidad.
+
+
+*/
+
